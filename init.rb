067bab7712e8d37d6da9722b9f7eddf383df7eaf
wisehackermonkey <oranbusiness@gmail.com>
2019-02-22 13:32:51 -0800

    version with no output

--- init.rb
+++ init.rb
@@ -11,7 +11,8 @@ header = nil
 Csv_data = []
 
 run_small_file = false
-large_csv_name = "real_data-100k.csv"
+# large_csv_name = "real_data-100k.csv"
+large_csv_name = "real_data-1000k.csv"
 file_name = run_small_file ? "real_customer_data.csv" : large_csv_name
 csv_file = File.join(Dir.pwd, file_name)
 puts "CSV FileFile Opened: #{csv_file}"
@@ -20,23 +21,26 @@ puts "CSV FileFile Opened: #{csv_file}"
 # read 50 lines then stop delete those 50 lines
 #   continue
 def fake_api_call(json)
-  puts json.join("  ")
-
+  # puts json.join("  ")
+  json.join("  ")
 end
 
 line_number = 0
 temp_line = []
-interval = 50
+interval = 1000
 CSV.foreach(csv_file) do |line|
-   line_number += 1
-    next if line_number == 1
-    if line_number % interval == 0
-      puts "ran for 50 records: line num #{line_number}, temp size #{temp_line.length},"
-      fake_api_call(temp_line)
-      temp_line = []
-    end
-    # break if line_number == 200
-    puts line_number.to_s + " | "
-    temp_line << line
+  line_number += 1
+  next if line_number == 1
+  if line_number % 10000 == 0
+    puts line_number
+  end
+  if line_number % interval == 0
+    # puts "ran for 50 records: line num #{line_number}, temp size #{temp_line.length},"
+    fake_api_call(temp_line)
+    temp_line = []
+  end
+  # break if line_number == 200
+  # puts line_number.to_s + " | "
+  temp_line << line
 end
 # puts temp_line
